436f2a7b03e6576bdc9c0ac2d361f3e32301c299
sabelo <67804592+SabeloMkhwanzi@users.noreply.github.com>
2022-05-16 00:35:17 +0200

    configured bridge get quote ui

--- component/GetQuotes/index.js
+++ component/GetQuotes/index.js
@@ -9,25 +9,36 @@ const FROM_TOKEN_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
 const TOKEN_AMOUNT = 10000;
 
-const FROM_CHAIN = "ethereum";
-const FROM_CHAIN_ID = 1;
+//const FROM_CHAIN = "ethereum";
+//const FROM_CHAIN_ID = 1;
 
 const TO_CHAIN = "polygon";
 const TO_CHAIN_ID = 137;
 
-const FROM_USE_ADDRESS = "0xF975206a46b4eD9f5F008AF9813B19bf083d94eE";
+//const fromUserAddress = "0xF975206a46b4eD9f5F008AF9813B19bf083d94eE";
 
 function GetQuotes() {
   const [items, setItems] = useState([]);
-  useEffect(() => {
-    getQuotes();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   getQuotes();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  const getQuotes = async () => {
+  const getQuotes = async (e) => {
     //Using fetch
+    // const chainId = e.target.elements.chainId.value;
+    // e.preventDefault();
+    const FROM_USER_ADDRESS = e.target.elements.FROM_USER_ADDRESS.value;
+    e.preventDefault();
+
+    //From Chain & chain id
+    const fromchain = e.target.elements.fromchain.value;
+    e.preventDefault();
+
+    const chainId = e.target.elements.chainId.value;
+    e.preventDefault();
     const response = await axios.get(
-      `https://swap.dev.swing.xyz/v0/transfer/quote?tokenSymbol=${TOKEN_SYMBOL}&tokenAmount=${TOKEN_AMOUNT}&fromTokenAddress=${FROM_TOKEN_ADDRESS}&fromChain=${FROM_CHAIN}&fromChainId=${FROM_CHAIN_ID}&toChain=${TO_CHAIN}&toChainId=${TO_CHAIN_ID}&fromUserAddress=${FROM_USE_ADDRESS}`,
+      `https://swap.dev.swing.xyz/v0/transfer/quote?tokenSymbol=${TOKEN_SYMBOL}&tokenAmount=${TOKEN_AMOUNT}&fromTokenAddress=${FROM_TOKEN_ADDRESS}&fromChain=${fromchain}&fromChainId=${FROM_CHAIN_ID}&toChain=${TO_CHAIN}&toChainId=${TO_CHAIN_ID}&fromUserAddress=${FROM_USER_ADDRESS}`,
       {
         method: "GET",
         headers: {
@@ -43,8 +54,9 @@ function GetQuotes() {
 
   return (
     <Box>
-      <Box border={1} borderColor="blue">
-        <BridgeUX getQuotes={GetQuotes} />
+      <Box>
+        {console.log(items)}
+        <BridgeUX getQuotes={getQuotes} />
       </Box>
     </Box>
   );
