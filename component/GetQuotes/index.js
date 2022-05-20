f7f8bc41b5186c065ea3c7841355844f50f18dbe
Seth McCullough <seth@endevrs.dev>
2022-05-20 13:46:16 -0400

    Fix bridge quote data results

--- component/GetQuotes/index.js
+++ component/GetQuotes/index.js
@@ -5,6 +5,7 @@ import BridgeUX from "./BridgeUX";
 import QuoteResult from "./QuotesResults";
 
 // Interface data
+
 const TOKEN_SYMBOL = "DAI";
 const FROM_TOKEN_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
@@ -20,45 +21,41 @@ const FROM_USER_ADDRESS = "0xF975206a46b4eD9f5F008AF9813B19bf083d94eE";
 
 function GetQuotes() {
   const [items, setItems] = useState([]);
+  console.log("ITEMS:", items);
   const InputTextColorMode = useColorModeValue("gray.900", "white");
+
+  const url = `https://swap.dev.swing.xyz/v0/transfer/quote?tokenSymbol=${TOKEN_SYMBOL}&tokenAmount=${TOKEN_AMOUNT}&fromTokenAddress=${FROM_TOKEN_ADDRESS}&fromChain=${FROM_CHAIN}&fromChainId=${FROM_CHAIN_ID}&toChain=${TO_CHAIN}&toChainId=${TO_CHAIN_ID}&fromUserAddress=${FROM_USER_ADDRESS}`;
+  const getQuotes = async (e) => {
+    e?.preventDefault();
+
+    try {
+      const { data } = await axios.get(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const items = data.routes?.map((item) => ({
+        duration: item?.duration,
+        amount: item?.quote?.amount,
+        bridgeFee: item?.quote?.bridgeFee,
+        bridge: item?.route[0]?.bridge,
+      }));
+
+      return setItems(items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     getQuotes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getQuotes = async (e) => {
-    //Using fetch
-    // const FROM_CHAIN = e.target.elements.FROM_CHAIN.value;
-    // e.preventDefault();
-
-    // const FROM_CHAIN_ID = e.target.elements.FROM_CHAIN_ID.value;
-    // e.preventDefault();
-
-    // const FROM_USER_ADDRESS = e.target.elements.FROM_USER_ADDRESS.value;
-    // e.preventDefault();
-
-    const response = await axios.get(
-      `https://swap.dev.swing.xyz/v0/transfer/quote?tokenSymbol=${TOKEN_SYMBOL}&tokenAmount=${TOKEN_AMOUNT}&fromTokenAddress=${FROM_TOKEN_ADDRESS}&fromChain=${FROM_CHAIN}&fromChainId=${FROM_CHAIN_ID}&toChain=${TO_CHAIN}&toChainId=${TO_CHAIN_ID}&fromUserAddress=${FROM_USER_ADDRESS}`,
-      // `https://swap.dev.swing.xyz/v0/transfer/quote?tokenSymbol=${TOKEN_SYMBOL}&tokenAmount=${TOKEN_AMOUNT}&fromTokenAddress=${FROM_TOKEN_ADDRESS}&fromChain=${FROM_CHAIN}&fromChainId=${FROM_CHAIN_ID}&toChain=${TO_CHAIN}&toChainId=${TO_CHAIN_ID}&fromUserAddress=${FROM_USER_ADDRESS}`,
-
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    // console.log(response.data);
-    setItems(response.data);
-
-    console.log(items);
-  };
-
   return (
     <Box>
       <Box>
-        {console.log(items)}
         <BridgeUX getQuotes={getQuotes} />
         <QuoteResult data={items} />
       </Box>
